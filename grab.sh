85b8292f9546511195adcae8cebc0b6f276c0676
olly <olly@ollygeorge.net>
2020-04-19 08:13:29 +0100

    Initial commit - simple py code, sh script to copy and convert

---
+++ grab.sh
@@ -0,0 +1,4 @@
+scp pi@192.168.1.140:/tmp/*.h264 /mnt/d/scratch/picam/raw
+for f in /mnt/d/scratch/picam/raw/*.h264; do MP4Box -add "$f" "$f.mp4";done
+mv /mnt/d/scratch/picam/raw/*.h264 /mnt/d/scratch/picam/rawdone
+mv /mnt/d/scratch/picam/raw/*.mp4 /mnt/d/scratch/picam/mp4
