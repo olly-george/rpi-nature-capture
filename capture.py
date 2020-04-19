387a4cb520de41d87a1172958129c7dab7680036
olly <olly@ollygeorge.net>
2020-04-19 14:21:21 +0100

    Rotated the cam 180 degrees

--- capture.py
+++ capture.py
@@ -5,7 +5,7 @@ camera = PiCamera()
 
 camera.resolution = (800, 600)
 camera.framerate = 20
-camera.rotation = 270
+camera.rotation = 90
 
 camera.start_preview()
 camera.start_recording('/home/pi/recordings/test3.h264')
