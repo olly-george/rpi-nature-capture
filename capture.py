9f25f93681fd6cf20c3472dcc56e42d5c992faab
olly <olly@ollygeorge.net>
2020-04-19 08:23:05 +0100

    rotate and changed framerate

--- capture.py
+++ capture.py
@@ -4,10 +4,11 @@ from time import sleep
 camera = PiCamera()
 
 camera.resolution = (800, 600)
-camera.framerate = 25
+camera.framerate = 20
+camera.rotation = 270
 
 camera.start_preview()
-camera.start_recording('/home/pi/recordings/test2.h264')
+camera.start_recording('/home/pi/recordings/test3.h264')
 sleep(5)
 camera.stop_recording()
 camera.stop_preview()
