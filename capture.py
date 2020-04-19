19fb18b92d93c63626d5f8f5100a4e27b095ea47
olly <olly@ollygeorge.net>
2020-04-19 08:18:05 +0100

    Changed resolution and paths

--- capture.py
+++ capture.py
@@ -3,8 +3,11 @@ from time import sleep
 
 camera = PiCamera()
 
+camera.resolution = (800, 600)
+camera.framerate = 25
+
 camera.start_preview()
-camera.start_recording('/home/pi/recordings/test.h264')
+camera.start_recording('/home/pi/recordings/test2.h264')
 sleep(5)
 camera.stop_recording()
 camera.stop_preview()
