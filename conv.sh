1647285627d4405d2de7b2e8b8b00af9741b24ec
olly <olly@ollygeorge.net>
2020-04-20 14:52:25 +0100

    quick change for testing

---
+++ conv.sh
@@ -0,0 +1,4 @@
+
+for f in /mnt/d/scratch/picam/raw/*.h264; do MP4Box -add "$f" "$f.mp4";done
+#mv /mnt/d/scratch/picam/raw/*.h264 /mnt/d/scratch/picam/rawdone
+#mv /mnt/d/scratch/picam/raw/*.mp4 /mnt/d/scratch/picam/mp4
--- grab.sh
+++ grab.sh
@@ -1,4 +1,4 @@
-scp pi@192.168.1.140:~/recordings/*.h264 /mnt/d/scratch/picam/raw
-for f in /mnt/d/scratch/picam/raw/*.h264; do MP4Box -add "$f" "$f.mp4";done
-mv /mnt/d/scratch/picam/raw/*.h264 /mnt/d/scratch/picam/rawdone
-mv /mnt/d/scratch/picam/raw/*.mp4 /mnt/d/scratch/picam/mp4
+scp pi@192.168.1.140:~/recordings/*.h264 /mnt/e/scratch/picam/raw
+for f in /mnt/e/scratch/picam/raw/*.h264; do MP4Box -add "$f" "$f.mp4";done
+mv /mnt/e/scratch/picam/raw/*.h264 /mnt/e/scratch/picam/rawdone
+mv /mnt/e/scratch/picam/raw/*.mp4 /mnt/e/scratch/picam/mp4
