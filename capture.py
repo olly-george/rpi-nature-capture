5d5b74401b89e5141ee9b3222de0bb02cdd2d2fd
olly <olly@ollygeorge.net>
2020-04-23 13:15:44 +0100

    ready to roll

--- capture.py
+++ capture.py
@@ -4,13 +4,13 @@ from time import sleep
 camera = PiCamera()
 
 camera.resolution = (800, 600)
-camera.framerate = 20
+camera.framerate = 25
 camera.rotation = 180
 
 camera.start_preview()
 
-for loop in range(1):
-    camera.start_recording('/home/pi/recordings/test' + str(loop) + '.h264')
-    sleep(5)
+for loop in range(200):
+    camera.start_recording('/home/pi/recordings/pond' + str(loop) + '.h264')
+    sleep(45)
     camera.stop_recording()
 camera.stop_preview()
